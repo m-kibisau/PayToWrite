83abb3a6a9465001171151029eeff575c466b514
m-kibisau <m.kibisau@andersenlab.com>
2020-02-18 16:48:58 +0300

    Created Persistence Layer and have tryed to create repasitory for the work with table of database

---
+++ PayToWrite.Persistence/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PayToWrite.Domain;
+
+namespace PayToWrite.Persistence
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Contacts> Contacts { get; set; }
+        public DbSet<Chats> Chats { get; set; }
+        public DbSet<Members> Members { get; set; }
+        public DbSet<Messages> Messages { get; set; }
+    }
+}
---
+++ PayToWrite.Persistence/EFGenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PayToWrite.Persistence
+{
+    public class EFGenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        DbContext _context;
+        DbSet<TEntity> _dbSet;
+
+        public EFGenericRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+        public void Create(TEntity item)
+        {
+            _dbSet.Add(item);
+            _context.SaveChanges();
+        }
+
+        public TEntity FindById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public IEnumerable<TEntity> Get()
+        {
+            return _dbSet.AsNoTracking().ToList();
+        }
+
+        public IEnumerable<TEntity> Get(Func<TEntity, bool> predicate)
+        {
+            return _dbSet.AsNoTracking().Where(predicate).ToList();
+        }
+
+        public void Remove(TEntity item)
+        {
+            _dbSet.Remove(item);
+            _context.SaveChanges();
+        }
+
+        public void Update(TEntity item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+    }
+}
